5e99bbbf398525d19df83e07a0eba4abf381f8d8
Amicsxoxo <onwukanjo.john@gmail.com>
2025-12-05 16:43:18 +0100

    commiting

---
+++ python-number_manuplation.py
@@ -0,0 +1,18 @@
+# print(8/3)
+
+
+# print(int(8/3))
+
+# print(round(8/3,2))
+
+# print(8//3)
+
+# result = 4 / 2
+# result /= 2
+# print(result)
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"Your score is {score}, your height is {height}, and your winning status is {isWinning}")
