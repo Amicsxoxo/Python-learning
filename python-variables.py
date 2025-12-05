564618bdae08e5e2e2618bf5d60a782dcf8db62d
Amicsxoxo <onwukanjo.john@gmail.com>
2025-12-05 12:34:25 +0100

    add all

---
+++ python-variables.py
@@ -0,0 +1,5 @@
+#print("Hello Mr "+ input("What is your surname ")+" "+ input("What is your firstname and middlename ") + " you're " + input("What is your age ") + "years old")
+
+name = input("What is your name? ")
+print("Hello " + name)
+
