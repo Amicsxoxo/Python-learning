81929b915dc2bac0c7d554f5ee19315405f2e930
Amicsxoxo <onwukanjo.john@gmail.com>
2026-02-16 17:54:32 +0100

    5:54 16/2/2026

--- picking_a_random_word_and_checking.py
+++ picking_a_random_word_and_checking.py
@@ -1,31 +1,61 @@
 import random
+from hangman_art import stages, hangman_logo
+from hangman_word_list import word_list
 
-word_list = ["ardvark", "baboon", "camel"]
+
+
+
+
 numb_of_words = len(word_list)
 #number of words in the list
 
 chosen_word = random.choice(word_list)
-print(f'The chosen word is {chosen_word}.')
+print(hangman_logo)
+
+lives = 6
 display = []
 word_length = len(chosen_word)
 for _ in range(word_length):
   display += "_"
+
+
+
+  # my style
   # print(display)
 
-list_of_chosen_word = []
-for letter in chosen_word:
-  list_of_chosen_word += letter
-# print(list_of_chosen_word)
+# list_of_chosen_word = []
+# for letter in chosen_word:
+#   list_of_chosen_word += letter
+# # print(list_of_chosen_word)
 
 
-while not list_of_chosen_word == display:
-  guess = input("Guess a letter: ").lower()
+# while not list_of_chosen_word == display:
+end_of_game = False
+while not end_of_game:
+  
+  guess = input("\nGuess a letter: ").lower()
+  if guess in display:
+    print(f"You've guessed {guess} before")
+
+  if guess not in chosen_word:
+    lives -= 1 
+    print(f"The letter {guess} is not in the word")
+  
 
   for position in range(word_length):
     letter = chosen_word[position]
     if letter == guess:
       display[position] = letter
-    
-  print(display)
 
-print("You have guessed the right word")
+   
+  print(display)
+  print(stages[lives])
+  
+  
+  if "_" not in display:
+    end_of_game = True
+    print("You won")
+  elif lives == 0:
+    end_of_game = True
+    print("You lost")
+
