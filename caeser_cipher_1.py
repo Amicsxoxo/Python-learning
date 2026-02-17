16c13f45c7c31e0e34eeaa8fc2fb9d2b72e2b889
Amicsxoxo <onwukanjo.john@gmail.com>
2026-02-17 13:33:56 +0100

    1:33 17/2/2026

---
+++ caeser_cipher_1.py
@@ -0,0 +1,37 @@
+from caeser_cipher_art import logo
+
+
+alphabet = ["a" , "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+print(logo)
+
+direction = input("Type 'encode' to encrypt, 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def caeser(plain_text, shift_amount,choice):
+  cipher_text = ""
+  if choice == "encode":
+    for letter in plain_text:
+      position = alphabet.index(letter)
+      new_position = position + shift_amount
+      if new_position > 25:
+        new_position = (new_position % 25 - 1)
+      new_letter = alphabet[new_position]
+      cipher_text += new_letter
+    print(f"The {choice}d text is {cipher_text}")
+  elif choice == "decode":
+    for letter in plain_text:
+      position = alphabet.index(letter)
+      new_position = position - shift_amount
+      if new_position < 0:
+        new_position = (new_position + 26)
+      new_letter = alphabet[new_position]
+      cipher_text += new_letter
+    print(f"The {choice}d text is {cipher_text}")
+  else:
+    print("Pls choose between 'encode' and 'decode'")
+  
+    
+caeser(shift_amount =  shift, plain_text = text, choice = direction)
