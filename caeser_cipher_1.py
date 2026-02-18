c46b741150f759453ef7c3c75fe2124648b5086e
Amicsxoxo <onwukanjo.john@gmail.com>
2026-02-18 17:17:15 +0100

    5:17 8/2/2026

--- caeser_cipher_1.py
+++ caeser_cipher_1.py
@@ -14,24 +14,40 @@ def caeser(plain_text, shift_amount,choice):
   cipher_text = ""
   if choice == "encode":
     for letter in plain_text:
-      position = alphabet.index(letter)
-      new_position = position + shift_amount
-      if new_position > 25:
-        new_position = (new_position % 25 - 1)
-      new_letter = alphabet[new_position]
+      if letter in alphabet:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        if new_position > 25:
+          new_position = (new_position % 25 - 1)
+        new_letter = alphabet[new_position]
+      elif letter not in alphabet:
+        new_letter = letter
       cipher_text += new_letter
     print(f"The {choice}d text is {cipher_text}")
   elif choice == "decode":
     for letter in plain_text:
-      position = alphabet.index(letter)
-      new_position = position - shift_amount
-      if new_position < 0:
-        new_position = (new_position + 26)
-      new_letter = alphabet[new_position]
+      if letter in alphabet:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        if new_position < 0:
+          new_position = (new_position + 26)
+        new_letter = alphabet[new_position]
+      elif letter not in alphabet:
+        new_letter = letter
       cipher_text += new_letter
     print(f"The {choice}d text is {cipher_text}")
   else:
     print("Pls choose between 'encode' and 'decode'")
+  game_status = input("Type 'yes' if you want to go again, Otherwise type 'no'\n").lower()
+  if game_status == "yes":
+    direction = input("Type 'encode' to encrypt, 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caeser(shift_amount =  shift, plain_text = text, choice = direction)
+  elif game_status == 'no':
+    print("Goodbye")
+
   
     
 caeser(shift_amount =  shift, plain_text = text, choice = direction)
+
