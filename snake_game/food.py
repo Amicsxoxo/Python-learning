1c83fea1999388ccaeacfb7b476d0592b5c87e8c
Amicsxoxo <onwukanjo.john@gmail.com>
2026-03-07 22:06:20 +0100

    additions

--- snake_game/food.py
+++ snake_game/food.py
@@ -1,18 +1,27 @@
 from turtle import Turtle
 import random
 
+
+
+def colour():
+  r = random.random()
+  g = random.random()
+  b = random.random()
+  return (r,g,b)
+
 class Food(Turtle):
   def __init__(self):
     super().__init__()
     self.shape("circle")
     self.penup()
     self.shapesize(stretch_len= 0.5, stretch_wid= 0.5)
-    self.color("blue")
+    self.color(colour())
     self.speed(0)
     self.refresh()
 
   def refresh(self):
     random_x = random.randint(-280,280)
     random_y = random.randint(-280,280)
+    self.color(colour())
     self.goto(random_x,random_y)
     
