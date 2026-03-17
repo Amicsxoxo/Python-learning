86b0c66f72d7ac8542faa9aea5ec99318166a279
Amicsxoxo <onwukanjo.john@gmail.com>
2026-03-17 14:30:59 +0100

    2:30 17/3/2026

--- day-27/main.py
+++ day-27/main.py
@@ -1,15 +1,28 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Project")
 window.minsize(width=500 , height= 300)
 
-my_label = tkinter.Label(text= "I am a Label", font= ('Arial', 24, "bold"))
+my_label = Label(text= "I am a Label", font= ('Arial', 24, "bold"))
 my_label.pack()
 
 my_label["text"] = "New label"
 my_label.config(text= "New label_2")
 
+
+def button_clicked():
+  my_label.config(text= input.get())
+  print("Button clicked")
+
+
+#Button
+button = Button(text="Pick me" ,command= button_clicked)
+button.pack()
+
+#Entry
+input = Entry(width= 10)
+input.pack()
 
 
 window.mainloop()
